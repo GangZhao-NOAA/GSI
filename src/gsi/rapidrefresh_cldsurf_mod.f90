00dca677df47ba48938ffcbdb28926d9ad507c87
Gang Zhao <gang.zhao@noaa.gov>
2026-04-24 05:42:16 +0000

    Minor midifications to GSI source code 1. set i_gsd_terrain_match_mesonet=1 to apply terrain match to    kx=188, 192, 193 and 195, and remove    i_gsd_terrain_match_mesonet=2 in gsd_terrain_match_surfTobs.f90

--- src/gsi/rapidrefresh_cldsurf_mod.f90
+++ src/gsi/rapidrefresh_cldsurf_mod.f90
@@ -254,8 +254,7 @@ module rapidrefresh_cldsurf_mod
 !      i_gsd_terrain_match_mesonet - namelist integer, control application of GSD Terrain Match to MESONET (MSO)
 !                                observations of Temp (188, 195)
 !                          = 0 : do not apply GSD terrain match to MESONET Obs of T (default)
-!                          = 1 : apply GSD terrain match to MESONET Obs of T (kx=188)
-!                          = 2 : apply GSD terrain match to MESONET Obs of T (kx=188/192/193/195)
+!                          = 1 : apply GSD terrain match to MESONET Obs of T (kx=188/192/193/195)
 !
 ! attributes:
 !   language: f90
